d08483911eff16336082f4d4f353a90003763b6c
n18l <github@n18l.com>
2026-04-10 16:15:31 -0700

    Fix Eleventy deployment issues

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,4 +1,10 @@
+import yaml from "js-yaml";
+
 export default function (eleventyConfig) {
+  eleventyConfig.addDataExtension("yml,yaml", (contents) =>
+    yaml.load(contents),
+  );
+
   eleventyConfig.addPassthroughCopy("src/images");
   eleventyConfig.addPassthroughCopy("src/*.css");
   eleventyConfig.addPassthroughCopy("src/favicon.*");
